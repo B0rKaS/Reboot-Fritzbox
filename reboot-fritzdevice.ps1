1bd7806b04796f27519ab3e0174f254ea493baab
B0rKaS <103313739+B0rKaS@users.noreply.github.com>
2022-04-09 18:25:33 +0200

    Update reboot-fritzdevice.ps1

--- reboot-fritzdevice.ps1
+++ reboot-fritzdevice.ps1
@@ -12,7 +12,9 @@
 	PS> ./reboot-fritzdevice -option 1
 	PS> ./reboot-fritzdevice -option 4 -customFQDN fritz.repeater2
 .NOTES
-	Author: B0rKaS / License: CC0
+	Scipt-Author: B0rKaS
+	Module: CredentialManager, Author: Dave Garnar, Copyright: (c) 2016 Dave Garnar. All rights reserved.
+.DEPEND
 #>
 
 param([string]$option, [string]$customFQDN)
